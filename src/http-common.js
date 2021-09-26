6f1093efd73a8fa0623b3f30accf74cb1863f676
4NUB!S <xexjz0leechersteamofficialclub@gmail.com>
2021-09-26 18:52:22 +0700

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 export default axios.create({
-    baseURL: isDevelopment ? "http://localhost:9000/api" : 'https://vue-project-one.herokuapp.com/api',
+    baseURL: isDevelopment ? "http://localhost:9000/api" : '',
     headers: {
         "Content-type": "application/json"
     }
